2bde0668acd05333c0b9e082c75d06e9cd1e3655
naderkamelaponar <nader.kamel.the.programmer@gmail.com>
2022-04-21 17:04:51 +0200

    m

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <p>
           بسم الله الرحمن الرحيم
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        
         <a
           className="App-link"
           href="https://reactjs.org"
